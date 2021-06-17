295cfefaf91d823f594b2fc3907c2fe896460d1b
Kyle <kyleferry@icloud.com>
2021-06-17 17:39:37 +0100

    Objects destructured

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const forecast = require('./utils/forecast')
 
 const location = process.argv[2]
 
-geoCode(location, (error, data) => {
+geoCode(location, (error, { latitude, longitude, location } = {} ) => {
 
-    if(error) {
+    if (error) {
         return console.log('Geo Error:', error)
     }
-    if(!location) {
+    if (!location) {
         return console.log('Please provide a location')
     }
-    
-    forecast(data.latitude, data.longitude, data.location, (error, forecastData) => {
-        if(error) {
+
+    forecast(latitude, longitude, location, (error, forecastData) => {
+        if (error) {
             return console.log('Weather Error:', error)
         }
-        
-        console.log(data.location)
+
+        console.log(location)
         console.log(forecastData)
-})
+    })
 })
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,18 +1,25 @@
 const request = require('request')
 
 const forecast = (long, lat, location, callback) => {
+
     const url = `http://api.weatherstack.com/current?access_key=13c4028ef37218c05e7bb2a59cb1f74d&query=${long},${lat}`
 
-    request({url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
+
+        //// COntinue
 
         if(error){
             callback('Unable to connect to weather service', undefined)
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to sourch forecast', undefined)
         } else {
-            callback(undefined,`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature}C outside. It feels like ${response.body.current.feelslike}C `)
+            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature}C outside. It feels like ${body.current.feelslike}C `)
         }
     })
 }
+
+
+////
+
 
 module.exports = forecast
